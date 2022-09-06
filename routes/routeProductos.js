83f76db0a617851a845caae3fefe4617c130ec96
Carolagot <carolagot@openmouth.com.ar>
2022-09-06 14:03:57 -0300

    archivos editar y crear producto

--- routes/routeProductos.js
+++ routes/routeProductos.js
@@ -4,5 +4,7 @@ const productosController= require("../controllers/productosController");
 
 router.get("/productDetail", productosController.productDetail);
 router.get("/productCart", productosController.productCart);
+router.get("/editProduct", productosController.editProduct);
+router.get("/createProduct", productosController.createProduct);
 
 module.exports= router;
