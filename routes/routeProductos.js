bb24d8dd61a50b6a781d2d5c2d26b0ebe303415e
Carolagot <carolagot@openmouth.com.ar>
2022-09-06 22:32:31 -0300

    cambio edit

--- routes/routeProductos.js
+++ routes/routeProductos.js
@@ -5,4 +5,5 @@ const productosController= require("../controllers/productosController");
 router.get("/productDetail", productosController.productDetail);
 router.get("/productCart", productosController.productCart);
 
+
 module.exports= router;
