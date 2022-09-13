ffee1c6ad071fe0cd22d55e2d5d7335a0b08256e
CintiaYael <leivicin@gmail.com>
2022-09-13 10:37:18 -0300

    subo jsonusuarios y jsonproductos

--- app.js
+++ app.js
@@ -15,9 +15,13 @@
     app.set("view engine","ejs");
     app.set("views",path.join(__dirname,"./views"));
 
+
+
 	app.listen(3100, () => {
     console.log ("Servidor corriendo en el puerto 3100")
 });
+    app.use (express.urlencoded({exprended:true}));
+    app.use (express.json());
 
 	app.use("/", routeMain);//sirve para ir mas directo a las rutas y no tener que usar dentro de routes la palabra route.
     app.use("/usuarios", routeUsuario);
--- routes/routeUsuario.js
+++ routes/routeUsuario.js
@@ -1,8 +1,12 @@
 const express= require("express");
 const router= express.Router();
+const path = require ("path");
+const multer= require ("multer");
 const usuariosController= require("../controllers/usuariosController");
 
 router.get("/login", usuariosController.login);
 router.get("/register", usuariosController.register);
+
+router.post("/register", usuariosController.postRegister);
 
 module.exports= router;
