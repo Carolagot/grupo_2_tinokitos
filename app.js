b3e648cadae9003d13bccfab5605c9668260e74a
Carolagot <carolagot@openmouth.com.ar>
2022-09-30 13:59:58 -0300

    pagina404

--- app.js
+++ app.js
@@ -30,6 +30,10 @@
     app.use("/usuarios", routeUsuario);
     app.use("/productos", routeProductos);
     app.use ("/administrador", routeAdministrador);
+    app.use ((req,res,next) => {
+        res.status(404).render("error404");
+        next();
+    });
 
 
 
