a8a62df354f4fbf58bcaceefa49e1e04ad300f32
CintiaYael <leivicin@gmail.com>
2022-10-19 12:19:02 -0300

    crear nuevo usuario y hasheado de password

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const bcryptjs = require ("bcryptjs");
 
 const usuariosController = {
 
@@ -14,17 +15,20 @@ const usuariosController = {
         console.log(req.body)
         const usuariosJson = fs.readFileSync(path.join(__dirname, '../data/userBD.json'), 'utf-8');
         const usuarios = JSON.parse(usuariosJson);
+        const constraseñaHasheada = bcryptjs.hashSync (req.body.password, 10);
         const nuevoUsuario = {
             id: Date.now(),
             nombre: req.body.nombre,
             telefono: req.body.telefono,
             email: req.body.email,
-            contraseña: req.body.contraseña,
+            password: constraseñaHasheada, 
+            categoria: req.body.categoria,
+            fotosUsuarios: '/imagenes/fotosUsuarios/' + req.file.filename
         };
         usuarios.push(nuevoUsuario);
         const usuariosActualizadosJSON = JSON.stringify(usuarios);
         fs.writeFileSync(path.join(__dirname, '../data/userBD.json'), usuariosActualizadosJSON, 'utf8');
-        res.send('Usted ha sido registrado exitosamente!!');
+        res.redirect("/index");
     },
 }
 
--- routes/routeUsuario.js
+++ routes/routeUsuario.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 const path = require("path");
 const usuariosController = require("../controllers/usuariosController");
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../public/imagenes/fotosUsuarios'));
+    },
+    filename: function (req, file, cb) {
+        let fotoNew =  Date.now() + '-' + file.originalname;
+
+        cb(null, fotoNew);
+    }
+});
+const upload = multer({ storage: storage });
+
+
 router.get("/login", usuariosController.login);
 router.get("/register", usuariosController.register);
-router.post("/register", usuariosController.postRegister);
+router.post("/register", upload.single("fotosUsuarios"), usuariosController.postRegister);
 
 module.exports = router;
