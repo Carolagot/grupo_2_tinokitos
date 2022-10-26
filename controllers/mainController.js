fb6dec7bf24798ae420d9ea94789729a228be3b1
CintiaYael <leivicin@gmail.com>
2022-10-26 12:37:18 -0300

    subo cambios

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,21 +1,36 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const mainController = {
     home: function (req, res) {
+        const userEmail = req.cookies.email;
+        if (userEmail) {
+            const usuariosJson = fs.readFileSync(path.join(__dirname, '../data/userBD.json'), 'utf-8');
+            const usuarios = JSON.parse(usuariosJson);
+            const user = usuarios.find (usuarioActual => usuarioActual.email == userEmail)
+            if (user) {
+                res.render ("index", {usuario: user})
+            } else {
+                res.render ("index", {usuario: {tipo:"usuario"}})
+            }
+        }else {
+            res.render ("index", {usuario: {tipo:"usuario"}})
+        }
         res.render('index');
     },
- filosofia: function (req, res) {
-    res.render('filosofia');
-},    
-contactanos: function (req, res) {
-    res.render('contactanos');
-},
-headerAdministrador:function (req, res) {
-    const usuariosJson = fs.readFileSync(path.join(__dirname, '../data/userBD.json'), 'utf-8');
-    const usuarios = JSON.parse(usuariosJson);
-    const usuarioLogueado = //usuarios.find(usuarioActual => usuarioActual.id == id);?
-    res.render("header", {usuario: usuarioLogueado});
-}
+    filosofia: function (req, res) {
+        res.render('filosofia');
+    },
+    contactanos: function (req, res) {
+        const usuariosJson = fs.readFileSync(path.join(__dirname, '../data/userBD.json'), 'utf-8');
+        const usuarios = JSON.parse(usuariosJson);
+        const usuarioLogueado = req.cookies("email");
+
+        const usuarioCookie = usuarios.find(usuarioActual => usuarioActual.email == usuarioLogueado);
+        console.log(usuarioCookie)
+        res.render('contactanos');
+    },
 };
 
 module.exports = mainController;
--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const bcryptjs = require ("bcryptjs");
+const bcryptjs = require("bcryptjs");
 
 const usuariosController = {
     login: function (req, res) {
-        res.render("login");
+        res.render("login", {
+            usuario: {
+                tipo: "usuario"
+            }
+        });
     },
     register: function (req, res) {
         res.render("register");
@@ -14,38 +18,45 @@ const usuariosController = {
         console.log(req.body)
         const usuariosJson = fs.readFileSync(path.join(__dirname, '../data/userBD.json'), 'utf-8');
         const usuarios = JSON.parse(usuariosJson);
-        const constraseñaHasheada = bcryptjs.hashSync (req.body.password, 10);
+        const constraseñaHasheada = bcryptjs.hashSync(req.body.password, 10);
         const nuevoUsuario = {
             id: Date.now(),
             nombre: req.body.nombre,
             email: req.body.email,
             telefono: req.body.telefono,
-            password: constraseñaHasheada, 
+            password: constraseñaHasheada,
             categoria: req.body.categoria,
             fotosUsuarios: '/imagenes/fotosUsuarios/' + req.file.filename,
-            notificaciones:req.body.notificaciones,
-            tipo:"comprador"
+            notificaciones: req.body.notificaciones,
+            tipo: "comprador"
         };
         usuarios.push(nuevoUsuario);
         const usuariosActualizadosJSON = JSON.stringify(usuarios);
         fs.writeFileSync(path.join(__dirname, '../data/userBD.json'), usuariosActualizadosJSON, 'utf8');
-        res.redirect("/");
+        res.cookie("email", req.body.email, { maxAge: 10800 });
+        res.redirect("/login");
     },
     loginProcess: (req, res) => {
-        const userData = req.body;
-        const usersJSON = fs.readFileSync(path.join(__dirname, "../data/usersDB.json"), "utf-8");
+        const userData = req.body; //guarda los datos ingresados en el formulario de login
+        const usersJSON = fs.readFileSync(path.join(__dirname, "../data/userBD.json"), "utf-8"); //buscamos usuario en la base de datos
         const usuarios = JSON.parse(usersJSON);
 
-        const usuarioLogueado = usuarios.find(thisUser => thisUser.mail === userData.mail);
-        if (usuarioLogueado) {
-            let contraseñaCorrecta = bcryptjs.compareSync(userData.password, usuarioLogueado.password);
-            if (contraseñaCorrecta) {
-                res.redirect("/");
-            } else {
-                res.redirect("/usuarios/register")
+        const usuarioLogueado = usuarios.find(thisUser => thisUser.email === userData.mail); //busca el usuario con el email ingresado
+        console.log(usuarioLogueado)
+        if (usuarioLogueado) { // si se encontro el usuario con ese email..
+            
+          //  let contraseñaCorrecta = bcryptjs.compareSync(userData.password, usuarioLogueado.password);
+          let contraseñaCorrecta = userData.password==usuarioLogueado.password  //chequeamos si la contraseña es correcta
+          if (contraseñaCorrecta) { // si es correcta...
+                res.cookie("email", req.body.email, { maxAge: 10800 }); //creamos una cookie
+                res.redirect("/"); //redireccionamos al index
+            } else { //si no es correcta
+                res.redirect("/usuarios/login") //te envia a loguearte
             }
+        }else{ // si no se encontro usuario con ese email
+            res.redirect("/usuarios/login")
         }
+    }
 }
-  }
 
 module.exports = usuariosController;
